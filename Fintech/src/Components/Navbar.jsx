49bd7d1610a13338d98cdc0aace04dc9b55333ac
Akpamgbo Philip <nzubeakpamgbo>
2025-02-04 05:40:03 +0100

    completed the Navbar and added a ReadMe

---
+++ Fintech/src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+          return (
+                    <nav className="navbar">
+                              <div className="logo">
+                                        <div className="imgContainer"><img className='logoImage' src="/images/logo.png" alt="" srcset="" /></div>
+                                        <span>FinTrack</span>
+                              </div>
+                              <ul className="nav-links">
+                                        <li><a href="#about">About</a></li>
+                                        <li><a href="#features">Features</a></li>
+                                        <li><a href="#pricing">Pricing</a></li>
+                                        <li><a href="#blog">Blog</a></li>
+                              </ul>
+                              <button className="sign-in">Sign In</button>
+                    </nav>
+          );
+};
+
+
+export default Navbar;
--- Fintech/src/main.jsx
+++ Fintech/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css"; // Ensure global styles are imported
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="scroll-container">
+      <App />
+    </div>
+  </React.StrictMode>
+);
